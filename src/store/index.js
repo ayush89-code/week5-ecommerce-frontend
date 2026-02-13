331628527768ba62ee1b772f56fcf6961c0fdebb
Ayush <ayuumourya71@gmail.com>
2026-02-14 01:39:53 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "./cartSlice";
-import productSlice from "./productSlice";
-import userSlice from './userSlice'
+import { configureStore } from '@reduxjs/toolkit'
+import productSlice from './productSlice'
+import cartSlice from './cartSlice'
 import wishlistSlice from './wishlistSlice'
 import comparisonSlice from './comparisonSlice'
 
 export const store = configureStore({
   reducer: {
-    cart: cartSlice,
     products: productSlice,
-    user: userSlice,
+    cart: cartSlice,
     wishlist: wishlistSlice,
     comparison: comparisonSlice
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST']
+      }
+    })
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
